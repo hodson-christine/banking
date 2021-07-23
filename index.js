2a158062a3b91fa3a8129acb057464e6f674b9bf
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2021-07-23 16:16:17 +0100

    update

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-	res.send("hello world");
+	res.json({
+		message: "Welcome to the application",
+	});
 });
 
 const port =
 	process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
-
-app.listen(3000);
+const server = app.listen(port, function () {
+		console.log("Server listening on port " + port)
+	})
+// app.listen(3000);
