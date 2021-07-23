5dabb23336105bf1466628deff997562ff40f62a
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2021-07-23 14:32:36 +0100

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
-	res.send({ message: "hello world" });
+	res.status(200).json({ message: "hello world" });
 });
 
 app.listen(3000);
