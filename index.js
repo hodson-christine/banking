a6bd65b1af56d8ab91d9c007d9c919e67ba4e4c3
Charles Damascus Togah <87181520+hodson-christine@users.noreply.github.com>
2021-07-26 14:52:49 +0000

    update

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use(userRouter);
-
+ mongoConnect();
 const port =
   process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
-const server = app.listen(port, async function () {
-  await mongoConnect();
+const server = app.listen(port, function () {
+  
   console.log("Server listening on port " + port);
 });
