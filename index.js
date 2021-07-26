d9cda81accfbcb253ee5af826a1bad69c9c247bc
Charles Damascus Togah <87181520+hodson-christine@users.noreply.github.com>
2021-07-26 14:50:35 +0000

    update

--- index.js
+++ index.js
@@ -3,21 +3,17 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userModel = require("./models/user.js");
 const mongoConnect = require("./util/database");
-let userRouter = require("./routes/user")
+let userRouter = require("./routes/user");
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.use(userRouter)
-
-
+app.use(userRouter);
 
 const port =
   process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
-const server = app.listen(port, function () {
+const server = app.listen(port, async function () {
+  await mongoConnect();
   console.log("Server listening on port " + port);
-  mongoConnect();
 });
-
-
