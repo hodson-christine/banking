7130d3c52c2f1c5c92d25b6a92a870f23f37f948
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2021-08-02 12:03:54 +0100

    update

--- controller/user.js
+++ controller/user.js
@@ -1,42 +1,42 @@
 let userModel = require("../models/user");
 
 exports.createUser = async (req, res) => {
-  let { email } = req.body;
+	let { email } = req.body;
 
- await userModel.findOne({ email: email }).then((emailExists) => {
-    if (emailExists) {
-      res.send({ message: "email exists" });
-    } else {
-      let userData = req.body;
-      const savedUser = new userModel(userData);
-      savedUser.save().then((savedUser) => {
-        if (savedUser) {
-          res.status(200).send({ message: "saved successfully", savedUser });
-        } else {
-          res.status(400).send({ message: "not saved" });
-        }
-      });
-    }
-  });
+	await userModel.findOne({ email: email }).then((emailExists) => {
+		if (emailExists) {
+			res.send({ message: "email exists" });
+		} else {
+			let userData = req.body;
+			const savedUser = new userModel(userData);
+			savedUser.save().then((savedUser) => {
+				if (savedUser) {
+					res.status(200).send({ message: "saved successfully", savedUser });
+				} else {
+					res.status(400).send({ message: "not saved" });
+				}
+			});
+		}
+	});
 };
 
-exports.fetchUser = async (req, res) => {
- await  userModel.find().then((getUsers) => {
-    if (getUsers) {
-      res.status(200).send({ message: "all users", getUsers });
-    } else {
-      res.status(500).send({ message: "no user" });
-    }
-  });
+exports.fetchUser = (req, res) => {
+	userModel.find().then((getUsers) => {
+		if (getUsers) {
+			res.status(200).send({ message: "all users", getUsers });
+		} else {
+			res.status(500).send({ message: "no user" });
+		}
+	});
 };
 
 exports.login = async (req, res) => {
-  let { email, password } = req.body;
-  await userModel.findOne({ email, password }).then((results) => {
-   if(results){
-     res.status(200).send({message : "logged in ", results})
-   }else{
-    res.status(400).send({message : "not logged in"})
-   }
-  });
+	let { email, password } = req.body;
+	await userModel.findOne({ email, password }).then((results) => {
+		if (results) {
+			res.status(200).send({ message: "logged in ", results });
+		} else {
+			res.status(400).send({ message: "not logged in" });
+		}
+	});
 };
